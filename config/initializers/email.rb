53d6e7197239e13d4e4aea056362479a456b575e
Chris MacNaughton <chris@centaurisolutions.nl>
2018-01-15 21:55:35 +0100

    add sendgrid config

---
+++ config/initializers/email.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'apikey',
+  :password             =>  Rails.application.secrets.sendgrid_api_key,
+  :domain               =>  Setting.hostname,
+  :enable_starttls_auto  =>  true
+}
