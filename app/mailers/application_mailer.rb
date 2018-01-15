873f67422c1e361b507e0cbf55a1dd1f04bb919f
Chris MacNaughton <chris@centaurisolutions.nl>
2018-01-15 22:37:30 +0100

    remove action mailer defaults

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,15 @@
 
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
+  default host: Setting.tracker_hostname
   layout "mailer"
 end
+
+
+# class CustomDeviseMailer < Devise::Mailer
+#   def confirmation_instructions(record, token, opts={})
+#     @token = token
+#     opts[:from] = "Dynamic Sender <dynamic@foo.com>"
+#     devise_mail(record, :confirmation_instructions, opts)
+#   end
+# end
