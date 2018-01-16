2739eb0aea837914e3344fa8741e074572522803
Chris MacNaughton <chris@centaurisolutions.nl>
2018-01-16 10:13:07 +0100

    simplify sign-in again

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,10 +6,7 @@ class Admin::UsersController < AdminController
   def update
     respond_to do |format|
       if @model.update(model_params)
-        # binding.pry
-        if @model == current_user
-          bypass_sign_in @model
-        end
+        bypass_sign_in @model if @model == current_user
         format.html { redirect_to [:admin, @model], notice: "#{current_model.to_s} was successfully updated." }
         format.json { render :show, status: :ok, location: @model }
       else
