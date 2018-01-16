e9f2906f680fb3d49d883bfba4d408f853a82b46
Chris MacNaughton <chris@centaurisolutions.nl>
2018-01-16 09:41:52 +0100

    fix updating self in admin, clean up notices
    [Finished #154342891]

--- config/routes.rb
+++ config/routes.rb
@@ -6,28 +6,27 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
 
-  resources :torrents
-
-  resources :announcements
-
   devise_scope :user do
     authenticated :user do
       root "announcements#index", as: :root
-      # get "torrents", as: :torrents
-      namespace :admin do
-        get "/", to: "dashboard#index", as: "dashboard"
-        resources :categories
-        resources :users
-        resources :releases
-        resources :torrents
-
-        mount RailsSettingsUi::Engine, at: "settings"
-      end
     end
 
     unauthenticated do
       root "devise/sessions#new", as: :unauthenticated_root
     end
+  end
+
+  resources :torrents
+  resources :announcements
+  namespace :admin do
+    get "/", to: "dashboard#index", as: "dashboard"
+    resources :categories
+    resources :users
+    resources :releases
+    resources :torrents
+
+
+    mount RailsSettingsUi::Engine, at: "settings"
   end
 
   get ":torrent_pass/announce", to: "tracker#announce"
