45ecef4267dfc86c0210af322f112390f92c8cf3
Chris MacNaughton <chmacnaughton@gmail.com>
2018-01-16 09:48:01 +0100

    #154342891 (#6)
    * Configure open registration
    
    [Finished #154352032]
    
    * remove useless override of authenticate_user!
    
    [Finished #154342898]
    
    * fix updating self in admin, clean up notices
    
    [Finished #154342891]

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -4,7 +4,7 @@ module AdminHelper
   def admin_field_for(form, model, attr)
     content_tag(:div, class: "field") do
       form.label(attr[0]) +
-      form.send("#{attr[1].try(:[], :type) || "text_field"}", attr[0], attr[1])
+      form.send("#{attr[1].try(:delete, :type) || "text_field"}", attr[0], attr[1])
       # form.text_area(attr[0], attr[1])
     end
   end
