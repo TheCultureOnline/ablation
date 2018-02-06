d875282a842a2e9d8a5ce7024f8b1cf3645dd81d
Chris MacNaughton <chris@centaurisolutions.nl>
2018-02-06 11:05:20 +0100

    update style

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,72 +1,74 @@
-require 'net/http'
+# frozen_string_literal: true
+
+require "net/http"
 
 class Importer
-    def initialize base_url, **keyword_args
-        @base_url = base_url
-        @torrent = keyword_args[:torrent]
-        @detail = keyword_args[:detail] || "#{@torrent}?torrentid=%s"
-        @login = keyword_args[:login]
-        @login_check = keyword_args[:login_check] || @login
-        @search = keyword_args[:search]
-        @user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0'
-        @cookies = ""
+  def initialize(base_url, **keyword_args)
+    @base_url = base_url
+    @torrent = keyword_args[:torrent]
+    @detail = keyword_args[:detail] || "#{@torrent}?torrentid=%s"
+    @login = keyword_args[:login]
+    @login_check = keyword_args[:login_check] || @login
+    @search = keyword_args[:search]
+    @user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0"
+    @cookies = ""
 
-        login
-    end
+    login
+  end
 
-    def login_params
-        {}
-    end
-    
-    def login
-        return true if login_check
-        output = request(@login, login_params.to_query)
+  def login_params
+    {}
+  end
 
-        if login_success? output
-            all_cookies = output.get_fields('set-cookie')
-            cookies_array = Array.new
-            all_cookies.each { | cookie |
-                cookies_array.push(cookie.split('; ')[0])
-            }
-            @cookies = cookies_array.join('; ')
-            true
-        else
-            false
-        end
-    end
+  def login
+    return true if login_check
+    output = request(@login, login_params.to_query)
 
-    def login_check
-        login_success? request(@login_check)
+    if login_success? output
+      all_cookies = output.get_fields("set-cookie")
+      cookies_array = Array.new
+      all_cookies.each { | cookie |
+        cookies_array.push(cookie.split("; ")[0])
+      }
+      @cookies = cookies_array.join("; ")
+      true
+    else
+      false
     end
+  end
 
-    def login_success? response
-        true
-    end
+  def login_check
+    login_success? request(@login_check)
+  end
 
-    def uri(part)
-        URI(
-            [@base_url, part].join('/')
-        )
-    end
+  def login_success?(response)
+    true
+  end
 
-    def request url, data = nil, query = nil
-        parsed_url = uri(url)
-        parsed_url.query = query if query
-        headers = {}
-        headers['Referer'] = "#{parsed_url.scheme}://#{parsed_url.host}"
-        headers['User-Agent'] = @user_agent
-        # headers['Accept-encoding'] = 'gzip'
-        headers['Connection'] = 'keep-alive'
-        headers['Cache-Control'] = 'max-age=0'
-        headers['Cookie'] = @cookies
-        http = Net::HTTP.new(parsed_url.host, parsed_url.port)
-        if data.nil? || data.length == 0
-            request = Net::HTTP::Get.new(parsed_url.request_uri, headers)
-        else
-            request = Net::HTTP::Post.new(parsed_url.request_uri, headers)
-        end
-        request.body = data if data
-        http.use_ssl = parsed_url.scheme == "https"
-        http.request(request)
+  def uri(part)
+    URI(
+      [@base_url, part].join("/")
+    )
+  end
+
+  def request(url, data = nil, query = nil)
+    parsed_url = uri(url)
+    parsed_url.query = query if query
+    headers = {}
+    headers["Referer"] = "#{parsed_url.scheme}://#{parsed_url.host}"
+    headers["User-Agent"] = @user_agent
+    # headers['Accept-encoding'] = 'gzip'
+    headers["Connection"] = "keep-alive"
+    headers["Cache-Control"] = "max-age=0"
+    headers["Cookie"] = @cookies
+    http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+    if data.nil? || data.length == 0
+      request = Net::HTTP::Get.new(parsed_url.request_uri, headers)
+    else
+      request = Net::HTTP::Post.new(parsed_url.request_uri, headers)
     end
+    request.body = data if data
+    http.use_ssl = parsed_url.scheme == "https"
+    http.request(request)
+  end
 end
--- lib/p2p.rb
+++ lib/p2p.rb
@@ -1,101 +1,103 @@
-require 'importer'
-require 'open-uri'
+# frozen_string_literal: true
+
+require "importer"
+require "open-uri"
 
 class P2P < Importer
-    def initialize username, password, key
-        @username = username
-        @password = password
-        @key = key
-        super(
-            "https://passthepopcorn.me",
-            torrent: "torrents.php",
-            login: "ajax.php?action=login",
-            search: "search/%s/0/7/%d",
-        )
+  def initialize(username, password, key)
+    @username = username
+    @password = password
+    @key = key
+    super(
+        "https://passthepopcorn.me",
+        torrent: "torrents.php",
+        login: "ajax.php?action=login",
+        search: "search/%s/0/7/%d",
+    )
+  end
+
+  def search(query, page = 1)
+    params = {
+        json: "noredirect",
+        order_by: "relevance",
+        order_way: "descending",
+        page: page,
+    }
+    params[:searchstr] = query if query
+    resp = request(@torrent, nil, params.to_query)
+    JSON.parse resp.body
+  end
+
+  def freeleech(page = 1)
+    params = {
+        freetorrent: 1,
+        json: "noredirect",
+        order_by: "relevance",
+        order_way: "descending",
+        page: page,
+    }
+    resp = request(@torrent, nil, params.to_query)
+    JSON.parse resp.body
+  end
+
+  def import_freeleech(page = 1)
+    free = freeleech(page)
+    auth_key = free["AuthKey"]
+    free["Movies"].each do |release|
+      import(release, auth_key)
+    end
+  end
+
+  def import(torrent_result, auth_key)
+    release = Release.where(
+      category: Category.find_by(name: "Movies"),
+    )
+    if torrent_result["Year"]
+      release = release.where(release_metadata: {
+          name: "year",
+          value: torrent_result["Year"]
+      })
+    end
+    binding.pry if release.name == "year"
+    release = release.where(
+      name: torrent_result["Title"],
+    ).first_or_create!
+    [
+        ["year", "Year"],
+        ["covert art", "Cover"],
+        ["Imdb ID", "ImdbId"]
+    ].each do |internal_name, external_name|
+      d = ReleaseMetadatum.where(
+        release: release,
+          name: internal_name,
+      ).first_or_initialize
+      d.value = torrent_result[external_name]
+      d.save
     end
 
-    def search(query, page=1)
-        params = {
-            json: "noredirect",
-            order_by: "relevance",
-            order_way: "descending",
-            page: page,
-        }
-        params[:searchstr] = query if query
-        resp = request(@torrent, nil, params.to_query)
-        JSON.parse resp.body
+    torrent_result["Torrents"].each do |torrent_result|
+      torrent_url = "https://passthepopcorn.me/torrents.php?action=download&id=#{torrent_result["Id"]}&authkey=#{auth_key}&torrent_pass=#{@key}"
+      torrent = Torrent.from_url(torrent_url, release)
+      sleep 2
     end
+  end
 
-    def freeleech page=1
-        params = {
-            freetorrent: 1,
-            json: "noredirect",
-            order_by: "relevance",
-            order_way: "descending",
-            page: page,
-        }
-        resp = request(@torrent, nil, params.to_query)
-        JSON.parse resp.body
+  def login_params
+    {
+        'username': @username,
+        'password': @password,
+        'passkey': @key,
+        'keeplogged': "1",
+        'login': "Login"
+    }
+  end
+
+  def login_success?(output)
+    begin
+      res = JSON.parse output.body
+      res["Result"].downcase == "ok"
+    rescue
+      return false
     end
-
-    def import_freeleech page=1
-        free = freeleech(page)
-        auth_key = free["AuthKey"]
-        free["Movies"].each do |release|
-            import(release, auth_key)
-        end
-    end
-
-    def import torrent_result, auth_key
-        release = Release.where(
-            category: Category.find_by(name: 'Movies'),
-        )
-        if torrent_result["Year"]
-            release = release.where(release_metadata: {
-                name: "year",
-                value: torrent_result["Year"]
-            })
-        end
-        binding.pry if release.name == "year"
-        release = release.where(
-            name: torrent_result["Title"],
-        ).first_or_create!
-        [
-            ["year", "Year"],
-            ["covert art", "Cover"],
-            ["Imdb ID", "ImdbId"]
-        ].each do |internal_name, external_name|
-            d = ReleaseMetadatum.where(
-                release: release,
-                name: internal_name,
-            ).first_or_initialize
-            d.value = torrent_result[external_name]
-            d.save
-        end
-
-        torrent_result["Torrents"].each do |torrent_result|
-            torrent_url = "https://passthepopcorn.me/torrents.php?action=download&id=#{torrent_result["Id"]}&authkey=#{auth_key}&torrent_pass=#{@key}"
-            torrent = Torrent.from_url(torrent_url, release)
-            sleep 2
-        end
-    end
-
-    def login_params
-        {
-            'username': @username,
-            'password': @password,
-            'passkey': @key,
-            'keeplogged': '1',
-            'login': 'Login'
-        }
-    end
-
-    def login_success? output
-        begin
-            res = JSON.parse output.body
-            res['Result'].downcase == "ok"
-        rescue
-            return false
-        end
-    end
+  end
 end
